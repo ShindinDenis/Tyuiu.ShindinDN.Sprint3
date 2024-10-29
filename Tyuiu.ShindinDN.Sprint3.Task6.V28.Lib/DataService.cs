b565095d0698deeda6bb9e71d78b3b315add14a9
ShindinDN <viber.kat@outlook.com>
2024-10-30 04:27:52 +0500

    Task6

---
+++ Tyuiu.ShindinDN.Sprint3.Task6.V28.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ShindinDN.Sprint3.Task6.V28.Lib
+{
+    public class DataService : ISprint3Task6V28
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int num = 0;
+            for (int i = startValue;i<= stopValue; i++)
+            {
+                for (int j = 1; j <= 7; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        num++;
+                    }
+                }
+            }
+            return num;
+        }
+    }
+}
---
+++ Tyuiu.ShindinDN.Sprint3.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.ShindinDN.Sprint3.Task6.V28.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint3.Task6.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new DataService();
+            int startValue = 13;
+            int stopValue = 19;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            Assert.AreEqual(16, res);
+        }
+    }
+}
