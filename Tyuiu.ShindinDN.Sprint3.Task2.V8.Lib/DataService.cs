7043e218dd067c20e6e42e34d439cd4c9d74bc3f
ShindinDN <viber.kat@outlook.com>
2024-10-30 03:02:55 +0500

    Task2

---
+++ Tyuiu.ShindinDN.Sprint3.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ShindinDN.Sprint3.Task2.V8.Lib
+{
+    public class DataService : ISprint3Task2V8
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double MultiSeries = 1;
+            do
+            {
+                MultiSeries = MultiSeries * (1 / (Math.Pow((Math.Cos(5) + 1), 2)));
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(MultiSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.ShindinDN.Sprint3.Task2.V8/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ShindinDN.Sprint3.Task2.V8.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint3.Task2.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Шиндин Д. Н. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Шиндин Денис Николаевич | ИСТНб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
+            Console.WriteLine("* произведение ряда по формуле                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 1;
+            int stopValue = 15;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
