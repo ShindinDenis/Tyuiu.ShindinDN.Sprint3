0fc8ad963a4382514b3daaf62b1196078e279aa6
ShindinDN <viber.kat@outlook.com>
2024-10-30 05:28:11 +0500

    Task7

---
+++ Tyuiu.ShindinDN.Sprint3.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+using Tyuiu.ShindinDN.Sprint3.Task7.V7.Lib;
+
+
+namespace Tyuiu.ShindinDN.Sprint3.Task7.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+            double[] wait;
+            wait = new double[len];
+            wait[0] = 15.19;
+            wait[1] = 11.67;
+            wait[2] = 9;
+            wait[3] = 0;
+            wait[4] = 4.08;
+            wait[5] = 1.5;
+            wait[6] = -2.28;
+            wait[7] = -6.52;
+            wait[8] = -10.19;
+            wait[9] = -12.76;
+            wait[10] = -14.68;
+            double[] res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.ShindinDN.Sprint3.Task7.V7/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.ShindinDN.Sprint3.Task7.V7.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint3.Task7.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Шиндин Д. Н. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Шиндин Денис Николаевич | ИСТНб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|     X    |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     | {1,6:f2}   |", startValue, res[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+        }
+    }
+}
